a57a8be8cf6e7b73ec2e2df9f67034761cea2039
lmsand <lmsand99@icloud.com>
2024-03-22 05:35:00 -0500

    list of stocks

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList } from 'react-native';
 import { Text, View } from '@/components/Themed';
+import { Stack } from 'expo-router';
+import top5 from '../../../assets/data/top5.json'
+import StockListItem from '@/components/StockListItem';
 
 export default function TabOneScreen() {
+  const stocks = Object.values(top5) // array of values
+  console.log(stocks)
+
   return (
     <View style={styles.container}>
+      <Stack.Screen options={{ title: 'Stocks'}}/>
 
-
+      <FlatList
+        data={stocks}
+        renderItem={({ item }) => <StockListItem stock={item} />}
+        contentContainerStyle = {{ gap: 20, padding: 10 }}
+      />
     </View>
   );
 }
---
+++ src/components/StockListItem.tsx
@@ -0,0 +1,52 @@
+import { Text, View } from "./Themed";
+import { StyleSheet } from "react-native";
+import Colors from "@/constants/Colors";
+import { AntDesign } from "@expo/vector-icons";
+import { MonoText } from "./StyledText";
+
+type Stock = {
+  name: string;
+  symbol: string;
+  close: string;
+  percent_change: string;
+};
+
+type StockListItem = {
+  stock: Stock;
+};
+
+export default function StockListItem({ stock }: StockListItem) {
+  const change = Number.parseFloat(stock.percent_change)
+
+  return (
+    <View style={styles.container}>
+      {/* Left Container */}
+      <View style={{ flex: 1, gap: 5 }}>
+        <Text style={styles.symbol}>
+          {stock.symbol} <AntDesign name="staro" size={18} color="gray" />
+        </Text>
+        <Text style={{ color: "gray" }}>{stock.name}</Text>
+      </View>
+
+      {/* Right Container */}
+      <View style={{ alignItems: 'flex-end' }}>
+        <MonoText>${Number.parseFloat(stock.close).toFixed(1)}</MonoText>
+        <MonoText style={{ color: change > 0 ? 'green' : 'red' }}>
+          {change > 0 ? '+' : ''}
+          {change.toFixed(1)}%
+        </MonoText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row'
+  },
+  symbol: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: Colors.light.tint,
+  },
+});
