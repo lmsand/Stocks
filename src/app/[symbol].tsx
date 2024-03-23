f3c53e8f48c11d2bdcb74df677998833815d11a0
lmsand <lmsand99@icloud.com>
2024-03-23 00:09:09 -0500

    graph using react native graph

--- src/app/[symbol].tsx
+++ src/app/[symbol].tsx
@@ -3,6 +3,7 @@ import {Text, View} from '../components/Themed'
 import { useLocalSearchParams, Stack } from 'expo-router'
 import top5 from '../../assets/data/top5.json'
 import StockListItem from '@/components/StockListItem'
+import Graph from '@/components/Graph'
 
 const StockDetails = () => {
   const { symbol } = useLocalSearchParams()
@@ -17,6 +18,7 @@ const StockDetails = () => {
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: stock.symbol, headerBackTitleVisible: false }} />
       <StockListItem stock={stock} />
+      <Graph />
     </View>
   )
 }
